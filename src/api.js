d9432e96310d2f188be6608bcc6f2ea7d49fbd3c
Inseo (Rachel) <yunibaek@hotmail.com>
2024-09-17 15:00:20 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import { QUOTE_API_URL } from './constants';
 
 // Load randome quotes from Zen API. Cache the results and refetch if necessary.
 async function getZenQuotes () {
-    const response = await fetch(`${QUOTE_API_URL}/today`);
+    const response = await fetch(`${QUOTE_API_URL}`);
     const jsonData = await response.json();
     return jsonData
 }
