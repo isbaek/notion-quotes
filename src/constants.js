a9ce0503fb21b6c17c349707010bc8124909984f
Inseo (Rachel) <yunibaek@hotmail.com>
2024-09-17 14:59:37 +0900

    fix: constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const WEBSITE_URL = 'https://studiorach.com';
-const QUOTE_API_URL = 'https://zenquotes.io/api';
+const QUOTE_API_URL = 'https://random-quotes-freeapi.vercel.app/api/random';
 
 export {
     WEBSITE_URL,
