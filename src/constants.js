b6eec41b79a03b8c2ec6e9297391cbe59f1aa1b1
Inseo (Rachel) <yunibaek@hotmail.com>
2024-09-17 14:17:52 +0900

    Update QUOTE_API_URL

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const WEBSITE_URL = 'https://studiorach.com';
-const QUOTE_API_URL = 'https://api.quotable.io/random';
+const QUOTE_API_URL = 'https://zenquotes.io/api';
 
 export {
     WEBSITE_URL,
